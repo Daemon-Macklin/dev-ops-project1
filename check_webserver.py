bc1fb4be41abbd20b5210e71a1c653eedb6c083e
Daemon-Macklin <daemon.macklin@outlook.com>
2018-10-25 12:22:51 +0100

    Adding comments to check script

--- check_webserver.py
+++ check_webserver.py
@@ -9,27 +9,40 @@ import subprocess
 
 def checknginx():
   try:
+
+    #Command that lists process and filters out ones with nginx in them
     cmd = 'ps -A | grep nginx'
-   
+
+    #Running the command
     subprocess.run(cmd, check=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     print("Nginx Server IS running")
+    #Returning 0 indecating the server is running
     return 0
    
   except subprocess.CalledProcessError:
     print("Nginx Server IS NOT running")
+    #Returning 1 indecating the server is not running
     return 1
 
 def startnginx():
   try:
+    
+    #Command that starts nginx
     cmd = 'sudo service nginx start'
+
+    #Running command
     subprocess.run(cmd, check=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     print("Nginx started")
    
   except subprocess.CalledProcessError:
+
+    #If the command doesn't run return there is an error
     print("--- Error starting Nginx! ---")
     
 # Define a main() function.
 def main():
+
+    #If the checknginx function returns run, run the start nginx function
     if checknginx() == 1:
       startnginx()
       
